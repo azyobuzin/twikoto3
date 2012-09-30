62c69c9c3e9ff7d4488c836b9295f8db586fc89b
azyobuzin <azyobuzin@users.sourceforge.jp>
2012-09-30 19:34:27 +0900

    一歩前進

--- twikoto3/twittertext/regex.py
+++ twikoto3/twittertext/regex.py
@@ -36,11 +36,11 @@ LATIN_ACCENTS_CHARS = (
 
 URL_VALID_PRECEEDING_CHARS = "(?:[^A-Z0-9@＠$#＃\u202A-\u202E]|^)"
 
-URL_VALID_CHARS = "[a-zA-Z0-9" + LATIN_ACCENTS_CHARS + "]"
-URL_VALID_SUBDOMAIN = "(?:(?:" + URL_VALID_CHARS + "[" + URL_VALID_CHARS + "\\-_]*)?" + URL_VALID_CHARS + "\\.)"
-URL_VALID_DOMAIN_NAME = "(?:(?:" + URL_VALID_CHARS + "[" + URL_VALID_CHARS + "\\-]*)?" + URL_VALID_CHARS + "\\.)"
+URL_VALID_CHARS = "a-zA-Z0-9" + LATIN_ACCENTS_CHARS
+URL_VALID_SUBDOMAIN = "(?:(?:[" + URL_VALID_CHARS + "][" + URL_VALID_CHARS + "\\-_]*)?[" + URL_VALID_CHARS + "]\\.)"
+URL_VALID_DOMAIN_NAME = "(?:(?:[" + URL_VALID_CHARS + "][" + URL_VALID_CHARS + "\\-]*)?[" + URL_VALID_CHARS + "]\\.)"
 #Any non-space, non-punctuation characters. \p{Z} = any kind of whitespace or invisible separator.
-URL_VALID_UNICODE_CHARS = "[.[^!\\\"#\\$%&'\\(\\)\\*\\+,\\-\\./:;<=>\\?@\\[\\]\\^_`\\{\\|\\}~\\s\u2000-\u206F]]"
+URL_VALID_UNICODE_CHARS = "[^!\\\"#\\$%&'\\(\\)\\*\\+,\\-\\./:;<=>\\?@\\[\\]\\^_`\\{\\|\\}~\\s\\u2000-\\u206F]"
 
 URL_VALID_GTLD = "(?:(?:aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|xxx)(?=[a-zA-Z0-9]|$))"
 URL_VALID_CCTLD = "(?:(?:ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|ss|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)(?=[a-zA-Z0-9]|$))"
@@ -55,7 +55,7 @@ URL_VALID_DOMAIN = (
       URL_VALID_DOMAIN_NAME +                                 # e.g. twitter.com
       "(?:" + URL_VALID_GTLD + "|" + URL_PUNYCODE + ")" +
     ")" +
-    "|(?:" + "(?:https?://)" +
+    "|(?:" + "(?:(?<=http://)|(?<=https://))" +
       "(?:" +
         "(?:" + URL_VALID_DOMAIN_NAME + URL_VALID_CCTLD + ")" +  # protocol + domain + ccTLD
         "|(?:" +
@@ -65,13 +65,13 @@ URL_VALID_DOMAIN = (
       ")" +
     ")" +
     "|(?:" +                                                  # domain + ccTLD + '/'
-      URL_VALID_DOMAIN_NAME + URL_VALID_CCTLD + "(?:/)" +     # e.g. t.co/
+      URL_VALID_DOMAIN_NAME + URL_VALID_CCTLD + "(?=/)" +     # e.g. t.co/
     ")"
 )
 
 URL_VALID_PORT_NUMBER = "\\d+"
 
-URL_VALID_GENERAL_PATH_CHARS = "[a-z0-9!\\*';:=\\+,.\\$/%#\\[\\]\\-_~\\|&" + LATIN_ACCENTS_CHARS + "]"
+URL_VALID_GENERAL_PATH_CHARS = "[a-z0-9!\\*';:=\\+,\\.\\$/%#\\[\\]\\-_~\\|&" + LATIN_ACCENTS_CHARS + "]"
 """
     Allow URL paths to contain balanced parens
     1. Used in Wikipedia URLs like /Primer_(film)
